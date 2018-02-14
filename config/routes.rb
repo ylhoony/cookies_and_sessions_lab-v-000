b3c99b5ec82ec1cbe779f6708e43c60ec4f9b318
Hoon Lee <yl.hoony@gmail.com>
2018-02-14 19:23:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'pages#index'
+  post '/', to: "pages#create"
   resources :products, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
