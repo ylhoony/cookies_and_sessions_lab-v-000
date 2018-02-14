8c40a99c6fbc69fc4890735bd99b897c0f0cd4a3
Hoon Lee <yl.hoony@gmail.com>
2018-02-14 19:10:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    session[:cart] || []
+    if session[:cart]
+      session[:cart]
+    else
+      session[:cart] = []
+    end
   end
 
 
